34a4801f9aac5e9d28ddc1b464d08d9d89fd8a4a
sukh <navsukh@gmail.com>
2013-07-11 15:03:36 -0400

    .

--- read_fits.py
+++ read_fits.py
@@ -13,33 +13,32 @@ class Read_fits():
 
     def read_data(self):
         Data = fits.open(self.FileName,memmap=True)
-        tbdata = Data[1].data
-        i=0
+#        tbdata = Data[1].data
         good=np.array([])
 
         for name in col_names:
-            col=np.array(tbdata.field(self.col_names[i]))
+            col=np.array(Data[1].data.field(name)
             temp_good=np.array(col>-9999)
             good=good&temp_good
             self.data_out=np.vstack((self.data_out,col))
 
         Data.close()
-
+#        data_out=data_out[good==True]
         return self.data_out
 
-    def read_data(FileName,col_names):
+    def read_data2(FileName,col_names):
         Data = fits.open(FileName,memmap=True)
-        tbdata = Data[1].data
+ #       tbdata = Data[1].data
         data_out=[]
         good=np.array([])
         for name in col_names:
-            col=np.array(tbdata.field(name))
+            col=np.array(Data[1].data.field(name))
             temp_good=np.array(col>-9999)
-            self.data_out=np.vstack((self.data_out,col))
+            self.data_out=np.vstack((data_out,col))
             good=good&temp_good
 
         Data.close()
-
+#        data_out=data_out[good==True]
         return data_out
 
 
