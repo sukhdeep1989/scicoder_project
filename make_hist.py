7808e235df6dcc375f2ebb39f9830a2a370b6de4
JHY <jhyoon@JHY-Mac.local>
2013-07-11 16:45:15 -0400

    added

--- make_hist.py
+++ make_hist.py
@@ -1,20 +1,16 @@
 import matplotlib.pyplot as plt 
 import numpy as np
-import sys
+#import sys
 
 
-def make_hists(*args):
-	
-	hist_num = len(args)
-	
+def make_hist(input_data):
+	hist_num = len(input_data)
 	fig,ax = plt.subplots(hist_num,1,figsize=(10,10))
-	
+
 	for i in range(hist_num):
-		ax[i].hist(feh,40)
+		ax[i].hist(input_data[i,:],40)
 		ax[i].set_xlim(-4,1)
 		ax[i].set_xlabel('[FE/H]',fontsize=20)
-	
 		fig.subplots_adjust(hspace=0.3)
-	
-	
+	print 'done'	
 
