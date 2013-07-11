69ca550180d961ddcc47c724534cace62f726b53
ASNA ANSARI <asna.ansari@gmail.com>
2013-07-11 16:53:51 -0400

    added xmin and xmax

--- make_hist.py
+++ make_hist.py
@@ -8,9 +8,11 @@ def make_hist(input_data):
 	fig,ax = plt.subplots(hist_num,1,figsize=(10,10))
 
 	for i in range(hist_num):
+		xmin[i] = min(input_data[i,:])
+		xmax[i] = max(input_data[i,:])
 		ax[i].hist(input_data[i,:],40)
-		ax[i].set_xlim(-4,1)
-		ax[i].set_xlabel('[FE/H]',fontsize=20)
+		ax[i].set_xlim(xmin[i],xmax[i])
+		ax[i].set_xlabel(label,fontsize=20)
 		fig.subplots_adjust(hspace=0.3)
 	print 'done'	
 
