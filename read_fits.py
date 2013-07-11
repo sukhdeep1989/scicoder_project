680ad1990b9ac835d178de7e1c7db2e812137b75
sukh <navsukh@gmail.com>
2013-07-11 14:04:29 -0400

    bug fix

--- read_fits.py
+++ read_fits.py
@@ -11,13 +11,32 @@ class Read_fits():
         self.col_names=col_names
 
     def read_data(self):
-        Data = fits.open(FileName,memmap=True)
+        Data = fits.open(self.FileName,memmap=True)
         tbdata = Data[1].data
         for i in range(len(self.col_names)):
-            col=tbdata.field(col_names[i])
+            col=tbdata.field(self.col_names[i])
             temp_good=col>-9999
             if i==0 :
                 self.data_out=col
+                good=temp_good
+            else:
+                temp=(self.data_out,col)
+                self.data_out=np.column_stack(temp)
+                good= good & temp_good
+
+            Data.close()
+
+            return data_out
+
+    def read_data(FileName,col_names):
+        Data = fits.open(FileName,memmap=True)
+        tbdata = Data[1].data
+        data_out=[]
+        for i in range(len(col_names)):
+            col=tbdata.field(col_names[i])
+            temp_good=col>-9999
+            if i==0 :
+                data_out=col
                 good=temp_good
             else:
                 temp=(self.data_out,col)
