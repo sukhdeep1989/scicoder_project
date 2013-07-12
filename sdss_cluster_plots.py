1d16f6e7dfe20d4079c34ec3962432b88c74d089
ASNA ANSARI <asna.ansari@gmail.com>
2013-07-12 15:53:09 -0400

    cluster position lookup using astropy.coords

--- sdss_cluster_plots.py
+++ sdss_cluster_plots.py
@@ -1,17 +1,18 @@
 #!/anaconda/bin/python
 
-import astropy.units as u
-from astropy import coordinates as coord
+import astropy.coordinates as coord
 import pos_search
-import coord
 
 with open("../SciCoder.2013.07/data/sdss_clusters.txt") as f:
 
 	lines = f.readlines()
-	print lines
 	clusters = []
 	for line in lines[1:]:
 		clusters.append(line.split(',')[0])
 	
 	print clusters
+	
+	n = len(clusters)
+	c = [coord.GalacticCoordinates.from_name(ii) for ii in clusters]
+	
 	
